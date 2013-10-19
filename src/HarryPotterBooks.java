9cba3fd29ab2d6229094b38fa5b6c332ce563a92
compwron <wordymusic@gmail.com>
2013-10-19 00:35:09 -0700

    WIP failing on two books of same type

--- src/HarryPotterBooks.java
+++ src/HarryPotterBooks.java
@@ -6,11 +6,23 @@ import java.util.List;
 public class HarryPotterBooks {
     @Getter
     private final double totalPrice;
-        private HashMap<BookType, Integer> bookTypesCount = emptyBookTypes();
+    private final Integer numberOfDifferentBooks;
+    private HashMap<BookType, Integer> bookTypesCount = emptyBookTypes();
 
     public HarryPotterBooks(List<Book> books) {
         calculateBookTypes(books);
+        this.numberOfDifferentBooks = numberOfDifferentBookTypes(bookTypesCount);
         this.totalPrice = calculateTotalPrice(books);
+    }
+
+    private Integer numberOfDifferentBookTypes(HashMap<BookType, Integer> bookTypesCount) {
+        int numberOfBookTypes = 0;
+        for (BookType bookType : bookTypesCount.keySet()){
+            if (bookTypesCount.get(bookType) != 0){
+                numberOfBookTypes += 1;
+            }
+        }
+        return numberOfBookTypes;
     }
 
     private void calculateBookTypes(List<Book> books) {
@@ -29,12 +41,15 @@ public class HarryPotterBooks {
 
     private double calculateTotalPrice(List<Book> books) {
         double totalPrice = 0.0;
-        for (BookType bookType : bookTypesCount.keySet()) {
-            int numbersOfThisTypeOfBook = bookTypesCount.get(bookType);
-            double totalPriceWithoutDiscountForBookType = numbersOfThisTypeOfBook * Book.STANDARD_PRICE;
-            double salePriceOfAllBooksOfBookType = totalPriceWithoutDiscountForBookType * DiscountPercentages.get(numbersOfThisTypeOfBook);
-            totalPrice += salePriceOfAllBooksOfBookType;
+        for (BookType bookType : bookTypesCount.keySet()){
+            double priceOfOneDiscountedBook = bookTypesCount.get(bookType) * DiscountPercentages.get(numberOfDifferentBooks) * Book.STANDARD_PRICE;
+            double priceOfNonDiscountedBooks =  numberOfNonDiscountedBooks(bookType) * Book.STANDARD_PRICE;
+            totalPrice += priceOfOneDiscountedBook + priceOfNonDiscountedBooks;
         }
         return totalPrice;
+    }
+
+    private int numberOfNonDiscountedBooks(BookType bookType) {
+        return (bookTypesCount.get(bookType) - 1) > 0 ? bookTypesCount.get(bookType) - 1 : 0;
     }
 }
